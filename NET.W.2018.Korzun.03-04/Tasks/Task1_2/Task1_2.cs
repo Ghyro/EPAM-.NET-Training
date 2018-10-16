20a6e562cd7f00115a52159b4c76fc3e13d6603b
Kirill <kirill.korzun.97@gmail.com>
2018-10-16 14:04:12 +0300

    Add task1-2 (Euclid, Stein)

---
+++ NET.W.2018.Korzun.03-04/Tasks/Task1_2/Task1_2.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Tasks
+{
+    /// <summary>
+    /// Task 1 and task 2 (implementations)
+    /// </summary>
+    public class Task1_2
+    {
+        /// <summary>
+        /// Develop a class that allows you to perform GCD
+        /// calculations using the Euclidean algorithm for two, three, etc. integers
+        /// </summary>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <returns>Value</returns>
+        public static int AlgorithmEuclideanNative(int number1, int number2)
+        {
+            number1 = Math.Abs(number1);
+            number2 = Math.Abs(number2);
+
+            while (number1 != number2)
+            {
+                if (number1 > number2)
+                {
+                    number1 -= number2;
+                }
+
+                else
+                {
+                    number2 -= number1;
+                }
+            }
+
+            return number1;
+        }
+
+        /// <summary>
+        /// Classical implementation (two param)
+        /// </summary>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <returns>Value</returns>
+        public static int AlgorithmEuclidean(int number1, int number2) => AlgorithmEuclideanNative(number1, number2);
+
+        /// <summary>
+        /// Implementation algorithm with array
+        /// </summary>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <param name="numbers">Input array</param>
+        /// <returns>Value</returns>
+        public static int AlgorithmEuclidean(int number1, int number2, params int[] numbers)
+        {
+            ValidationEuclidean(numbers);
+
+            int outputResult = AlgorithmEuclideanNative(number1, number2);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                outputResult = AlgorithmEuclideanNative(outputResult, numbers[i]);
+            }
+
+            return outputResult;
+        }
+        
+        /// <summary>
+        /// Get time to classical implementation (two param)
+        /// </summary>
+        /// <param name="time">This object represents a time interval.</param>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <returns>Value</returns>
+        public static int AlgorithmEuclideanTime(out TimeSpan timeSpan, int number1, int number2)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            int outputResult = AlgorithmEuclidean(number1, number2);
+
+            stopwatch.Stop();
+
+            timeSpan = stopwatch.Elapsed;
+
+            return outputResult;
+        }
+
+        /// <summary>
+        /// Get time to implementation with array
+        /// </summary>
+        /// <param name="time">This object represents a time interval.</param>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <param name="numbers">Input array</param>
+        /// <returns>Value</returns>
+        public static int AlgorithmEuclideanTime(out TimeSpan timeSpan, int number1, int number2, params int[] numbers)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            int outputResult = AlgorithmEuclidean(number1, number2, numbers);
+
+            stopwatch.Stop();
+
+            timeSpan = stopwatch.Elapsed;
+
+            return outputResult;
+        }
+
+        /// <summary>
+        /// Check input array
+        /// </summary>
+        /// <param name="numbers">Input array</param>
+        /// <exception cref="ArgumentNullException">if input array = null.</exception>
+        public static void ValidationEuclidean(params int[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException("Array is null!");
+            }
+        }
+
+        /// <summary>
+        /// Add to the developed class methods that implement the Stein algorithm
+        /// (binary Euclidean algorithm) for calculating the GCD of two, three, etc. integers
+        /// </summary>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <returns>Value</returns>
+        public static int AlgorithmSteinNative(int number1, int number2)
+        {
+            number1 = Math.Abs(number1);
+
+            number2 = Math.Abs(number2);
+
+            if (number1 == number2)
+            {
+                return number1;
+            }
+
+            if (number1 == 0)
+            {
+                return number2;
+            }
+
+            if (number2 == 0)
+            {
+                return number1;
+            }
+
+            if ((~number1 & 1) != 0)
+            {
+                if ((number2 & 1) != 0)
+                {
+                    return AlgorithmSteinNative(number1 >> 1, number2);
+                }
+
+                else
+                {
+                    return AlgorithmSteinNative(number1 >> 1, number2 >> 1) << 1;
+                }
+            }
+
+            if ((~number2 & 1) != 0)
+            {
+                return AlgorithmSteinNative(number1, number2 >> 1);
+            }
+
+            if (number1 > number2)
+            {
+                return AlgorithmSteinNative((number1 - number2) >> number1, number2);
+            }
+
+            return AlgorithmSteinNative((number2 - number1) >> 1, number1);
+        }
+
+        /// <summary>
+        /// Stein implementation (two param)
+        /// </summary>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <returns>Value</returns>
+        public static int AlgorithmStein(int number1, int number2) => AlgorithmSteinNative(number1, number2);
+
+        /// <summary>
+        /// Stein implementation with array
+        /// </summary>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <param name="numbers">Input array</param>
+        /// <returns>Value</returns>
+        public static int AlgorithmStein(int number1, int number2, params int[] numbers)
+        {
+            ValidationStein(numbers);
+
+            int outputResult = AlgorithmSteinNative(number1, number2);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                outputResult = AlgorithmSteinNative(outputResult, numbers[i]);
+            }
+
+            return outputResult;
+        }
+
+        /// <summary>
+        /// Get time to implementation (two number)
+        /// </summary>
+        /// <param name="time">This object represents a time interval.</param>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <param name="numbers">Input array</param>
+        /// <returns>Value</returns>
+        public static int AlgorithmSteinTime(out TimeSpan timeSpan, int number1, int number2)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            int outputResult = AlgorithmStein(number1, number2);
+
+            stopwatch.Stop();
+
+            timeSpan = stopwatch.Elapsed;
+
+            return outputResult;
+        }
+
+        /// <summary>
+        /// Get time to implementation with array
+        /// </summary>
+        /// <param name="time">This object represents a time interval.</param>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <param name="numbers">Input array</param>
+        /// <returns>Value</returns>
+        public static int AlgorithmSteinTime(out TimeSpan timeSpan, int number1, int number2, params int[] numbers)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            int outputResult = AlgorithmStein(number1, number2);
+
+            stopwatch.Stop();
+
+            timeSpan = stopwatch.Elapsed;
+
+            return outputResult;
+        }
+
+        /// <summary>
+        /// Check input array
+        /// </summary>
+        /// <param name="numbers">Input array</param>
+        /// <exception cref="ArgumentNullException">if input array = null.</exception>
+        public static void ValidationStein(params int[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException("Array is null!");
+            }
+        }
+
+    }
+}
---
+++ NET.W.2018.Korzun.03-04/TasksTests/Task1_2/Task1_2Tests.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using Tasks;
+
+namespace TasksTests
+{
+    [TestFixture]
+    public class Task1_2Tests
+    {
+        [Test]
+        [TestCase(6, 12, 7, 55, ExpectedResult = 1)]
+        [TestCase(24, 3, 48, 213, ExpectedResult = 3)]
+        [TestCase(20, 40, 48, ExpectedResult = 4)]
+        public int AlgorithmEuclidean_InputCorrentValuesParams_NUnit(int number1, int number2, params int[] numbers)
+        {
+            //Assert
+            return Task1_2.AlgorithmEuclidean(number1, number2, numbers);
+        }
+
+        [Test]
+        [TestCase(20, 40, ExpectedResult = 20)]
+        [TestCase(5, 40, ExpectedResult = 5)]
+        [TestCase(124, 40, ExpectedResult = 4)]
+        [TestCase(24, 123, ExpectedResult = 3)]
+        [TestCase(22, 44, ExpectedResult = 22)]
+        public int AlgorithmEuclidean_InputCorrentValuesTwoNumber_NUnit(int number1, int number2)
+        {
+            //Assert
+            return Task1_2.AlgorithmEuclidean(number1, number2);
+        }
+
+        [Test]
+        public void AlgorithmEuclidean_InputNull_NUnit()
+        {
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => Task1_2.AlgorithmEuclidean(6, 12, null));
+        }
+
+        [Test]
+        [TestCase(20, 40)]
+        [TestCase(5, 40)]
+        [TestCase(124, 40)]
+        [TestCase(24, 123)]
+        [TestCase(22, 44)]
+        public void AlgorithmEuclideanTime_GetTimeTwoNumber_NUnit(int number1, int number2)
+        {
+            //Arrange
+            TimeSpan timeSpan = new TimeSpan();
+
+            //Act
+            int result = Task1_2.AlgorithmEuclideanTime(out timeSpan, number1, number2);
+
+            //Assert
+            Assert.IsTrue(timeSpan.Seconds <= 1, $"Time is {timeSpan.ToString()}");
+        }
+
+        [Test]
+        [TestCase(6, 12, 7, 55)]
+        [TestCase(24, 3, 48, 213)]
+        [TestCase(20, 40, 48)]
+        public void AlgorithmEuclideanTime_GetTimeParam_NUnit(int number1, int number2, params int[] numbers)
+        {
+            //Arrange
+            TimeSpan timeSpan = new TimeSpan();
+
+            //Act
+            int result = Task1_2.AlgorithmEuclideanTime(out timeSpan, number1, number2, numbers);
+
+            //Assert
+            Assert.IsTrue(timeSpan.Seconds <= 1, $"Time is {timeSpan.ToString()}");
+        }
+
+        [Test]
+        [TestCase(6, 12, 7, 55, ExpectedResult = 1)]
+        [TestCase(24, 3, 48, 213, ExpectedResult = 3)]
+        [TestCase(4, 16, 8, ExpectedResult = 4)]
+        [TestCase(5, 40, 20, ExpectedResult = 5)]
+        public int AlgorithmStein_InputCorrentValuesParams_NUnit(int number1, int number2, params int[] numbers)
+        {
+            //Assert
+            return Task1_2.AlgorithmStein(number1, number2, numbers);
+        }
+
+        [Test]
+        [TestCase(20, 40, ExpectedResult = 20)]
+        [TestCase(5, 40, ExpectedResult = 5)]
+        [TestCase(24, 123, ExpectedResult = 3)]
+        [TestCase(22, 44, ExpectedResult = 22)]
+        public int AlgorithmStein_InputCorrentValuesTwoNumber_NUnit(int number1, int number2)
+        {
+            //Assert
+            return Task1_2.AlgorithmStein(number1, number2);
+        }
+
+        [Test]
+        public void AlgorithmStein_InputNull_NUnit()
+        {
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => Task1_2.AlgorithmEuclidean(6, 12, null));
+        }
+
+        [Test]
+        [TestCase(20, 40)]
+        [TestCase(5, 40)]
+        [TestCase(124, 40)]
+        [TestCase(24, 123)]
+        [TestCase(22, 44)]
+        public void AlgorithmSteinTime_GetTimeTwoNumber_NUnit(int number1, int number2)
+        {
+            //Arrange
+            TimeSpan timeSpan = new TimeSpan();
+
+            //Act
+            int result = Task1_2.AlgorithmSteinTime(out timeSpan, number1, number2);
+
+            //Assert
+            Assert.IsTrue(timeSpan.Seconds <= 1, $"Time is {timeSpan.ToString()}");
+        }
+
+        [Test]
+        [TestCase(6, 12, 7, 55)]
+        [TestCase(24, 3, 48, 213)]
+        [TestCase(20, 40, 48)]
+        public void AlgorithmSteinTime_GetTimeParam_NUnit(int number1, int number2, params int[] numbers)
+        {
+            //Arrange
+            TimeSpan timeSpan = new TimeSpan();
+
+            //Act
+            int result = Task1_2.AlgorithmSteinTime(out timeSpan, number1, number2, numbers);
+
+            //Assert
+            Assert.IsTrue(timeSpan.Seconds <= 1, $"Time is {timeSpan.ToString()}");
+        }
+    }
+}
