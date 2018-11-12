7583b57889a8d2c33fb780291543bdbfec761618
Kirill <kirill.korzun.97@gmail.com>
2018-11-12 22:33:53 +0300

    add task1 (day 12-13)

--- NET.W.2018.Korzun.08/Book.ConsoleTest/Program.cs
+++ NET.W.2018.Korzun.08/Book.ConsoleTest/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,24 +11,56 @@ namespace BookConsoleTest
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            logger.Debug("The program has been started.");
+
+            logger.Debug("Attempt to create objects");
+
             IBookListStorage bookListStorage = new BookListToBinary("storage.dat");
 
             IBookListService bookService = new BookService(bookListStorage);
 
-            bookService.AddBook(new Book("918-1-16197-276-1", "Джеффри Рихтер", "CLR via C#", "Питер", 2016, 896, 55));
-            bookService.AddBook(new Book("928-2-26197-376-2", "Джон Скит", "C# программирование для профессионалов", "Вильямс", 2018, 608, 68));
-            bookService.AddBook(new Book("938-3-36197-476-3", "Марк Дж. Прайс", "C# 7 и .NET Core.", "Питер", 2018, 640, 75));
-            bookService.AddBook(new Book("948-4-46197-576-4", "Эндрю Троелсен", "Язык программирования C# 6.0", "Вильямс", 2017, 1440, 123));
-            bookService.AddBook(new Book("958-5-56197-676-5", "Герберт Шилдт", "C# 4.0. Полное руководство", "Вильямс", 2015, 1056, 60));
+            logger.Debug($"Objects have been created.");
+
+            logger.Debug($"Attempt to add new books...");
+            try
+            {
+                logger.Info("Adding new books...");
+
+                bookService.AddBook(new Book("918-1-16197-276-1", "Джеффри Рихтер", "CLR via C#", "Питер", 2016, 896, 55));
+                bookService.AddBook(new Book("928-2-26197-376-2", "Джон Скит", "C# программирование для профессионалов", "Вильямс", 2018, 608, 68));
+                bookService.AddBook(new Book("938-3-36197-476-3", "Марк Дж. Прайс", "C# 7 и .NET Core.", "Питер", 2018, 640, 75));
+                bookService.AddBook(new Book("948-4-46197-576-4", "Эндрю Троелсен", "Язык программирования C# 6.0", "Вильямс", 2017, 1440, 123));
+                bookService.AddBook(new Book("958-5-56197-676-5", "Герберт Шилдт", "C# 4.0. Полное руководство", "Вильямс", 2015, 1056, 60));
+
+                logger.Info("New books have been added.");
+            }
+            catch(Exception e)
+            {
+                logger.Warn("Book has not been added to list.");
+            }
+            
 
             var findBook = new List<Book>();
             findBook.Add(bookService.FindBookByTag("CLR via C#"));
 
             GetFindBook(findBook);
 
-            bookService.SaveBooskInStorage();
+            logger.Debug($"Attempt to save book list...");
+
+            try
+            {
+                logger.Info("Save books...");
+                bookService.SaveBooskInStorage();
+            }
+            catch(IOException e)
+            {
+                logger.Warn("Failed to save book list");
+            }
+            
 
             Console.ReadLine();
         }
--- NET.W.2018.Korzun.08/Tasks/Task1/BookServices/BookService .cs
+++ NET.W.2018.Korzun.08/Tasks/Task1/BookServices/BookService .cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,6 +48,7 @@ namespace Tasks.Task1
         /// </exception>
         public void AddBook(Book book)
         {
+            
             if (book is null)
             {
                 throw new ArgumentNullException($"{nameof(book)} is null.");
