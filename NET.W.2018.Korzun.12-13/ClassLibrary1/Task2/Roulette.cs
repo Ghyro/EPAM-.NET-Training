803214e3507463e4e166cbac3ca98805ba54fc3c
Kirill <kirill.korzun.97@gmail.com>
2018-11-13 22:48:15 +0300

    Add task2 (day 12-13)

---
+++ NET.W.2018.Korzun.12-13/ClassLibrary1/Task2/Roulette.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks.Task2
+{
+    /// <summary>
+    /// Class <see cref="RouletteEventArgs">consist two values for storage <see cref="Message">
+    /// and <see cref="Number"/>/></see>/>
+    /// </summary>
+    public class RouletteEventArgs : EventArgs
+    {      
+        public RouletteEventArgs(string message, int number)
+        {
+            this.Message = message;
+            this.Number = number;
+        }
+
+        public string Message { get; }
+
+        public int Number { get; }
+    }
+
+    /// <summary>
+    /// The implementation of the simulation game Roulette
+    /// </summary>
+    public class Roulette
+    {   
+        Random random = new Random();
+
+        public delegate void RouletteStateHandler(object sender, RouletteEventArgs e);
+
+        public event RouletteStateHandler Win;
+
+        public event RouletteStateHandler Lose;
+
+        public void Game(int number)
+        {
+            if (number > 36 || number < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number));
+            }
+
+            if (ReferenceEquals(number, null))
+            {
+                throw new ArgumentNullException(nameof(number));
+            }
+
+            int result = random.Next(0, 36);
+
+            Console.WriteLine("Roulette rotates...");
+            System.Threading.Thread.Sleep(1500);
+
+            if (result == number)
+            {
+                Win?.Invoke(this, new RouletteEventArgs($"You are winner!!! Dropped number: {result}", number));
+            }
+            else
+            {
+                Lose?.Invoke(this, new RouletteEventArgs($"You are loser!!! Dropped number: {result}", number));
+            }
+        }        
+    }
+}
---
+++ NET.W.2018.Korzun.12-13/RouletteConsoleTest/RouletteConsole.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasks.Task2;
+
+namespace RouletteConsoleTest
+{
+    class RouletteConsole
+    {
+        static void Main()
+        {
+            Roulette roulette = new Roulette();
+
+            roulette.Win += WinMessage;
+            roulette.Lose += LoseMessage;
+
+            int k = 3;
+
+            while (k >= 0)
+            {
+                Console.WriteLine("Enter your number for the game (number from 0 to 36)...");
+
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                roulette.Game(number);
+
+                Console.WriteLine($"Number of attempts: {k}");
+
+                --k;
+            }
+
+            Console.WriteLine("Closing the program...");
+
+            System.Threading.Thread.Sleep(2000);
+        }
+
+        private static void WinMessage(object sender, RouletteEventArgs e)
+        {
+            Console.Write(e.Message);
+            Console.WriteLine($" Your number: {e.Number}");
+        }
+
+        private static void LoseMessage(object sender, RouletteEventArgs e)
+        {
+            Console.Write(e.Message);
+            Console.WriteLine($" Your number: {e.Number}");
+        }
+
+    }
+}
