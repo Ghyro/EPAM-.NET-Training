a43d4704fb680c9909ca3fefa7f94a9df94cb282
Kirill <kirill.korzun.97@gmail.com>
2018-11-28 01:07:54 +0300

    comments no1

--- DebtsTraining/No1/PasswordCheckerService.cs
+++ DebtsTraining/No1/PasswordCheckerService.cs
@@ -8,35 +8,35 @@ namespace No1
 {
     public class PasswordCheckerService
     {
-        private readonly SqlRepository repository = new SqlRepository();
+        //private readonly SqlRepository repository = new SqlRepository();
 
-        public (bool, string) VerifyPassword(string password)
-        {
-            if (password == null)
-                throw new ArgumentException($"{password} is null arg");
+        //public (bool, string) VerifyPassword(string password)
+        //{
+        //    if (password == null)
+        //        throw new ArgumentException($"{password} is null arg");
 
-            if (password == string.Empty)
-                return (false, $"{password} is empty ");
+        //    if (password == string.Empty)
+        //        return (false, $"{password} is empty ");
 
-            // check if length more than 7 chars 
-            if (password.Length <= 7)
-                return (false, $"{password} length too short");
+        //    // check if length more than 7 chars 
+        //    if (password.Length <= 7)
+        //        return (false, $"{password} length too short");
 
-            // check if length more than 10 chars for admins
-            if (password.Length >= 15)
-                return (false, $"{password} length too long");
+        //    // check if length more than 10 chars for admins
+        //    if (password.Length >= 15)
+        //        return (false, $"{password} length too long");
 
-            // check if password contains at least one alphabetical character 
-            if (!password.Any(char.IsLetter))
-                return (false, $"{password} hasn't alphanumerical chars");
+        //    // check if password contains at least one alphabetical character 
+        //    if (!password.Any(char.IsLetter))
+        //        return (false, $"{password} hasn't alphanumerical chars");
 
-            // check if password contains at least one digit character 
-            if (!password.Any(char.IsNumber))
-                return (false, $"{password} hasn't digits");
+        //    // check if password contains at least one digit character 
+        //    if (!password.Any(char.IsNumber))
+        //        return (false, $"{password} hasn't digits");
 
-            repository.Create(password);
+        //    repository.Create(password);
 
-            return (true, "Password is Ok. User was created");
-        }
+        //    return (true, "Password is Ok. User was created");
+        //}
     }
 }
